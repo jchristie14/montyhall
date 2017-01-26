f77fbae8e44294e0165bad1a8038863af3093d4c
John Christie <omeara.christie@gmail.com>
2017-01-26 13:28:36 -0500

    Add door opener

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,11 @@ function Game() {
   this.car = this.randDoor();
   this.goat = [1, 2, 3].filter(el => el !== this.car);
   this.choice = this.randDoor();
-  this.doorsThatCanBeOpened = [1, 2, 3].filter(el => el !== this.choice).filter(el => el !== this.car);
-  this.play = console.log(`car is ${this.car}; goats are ${this.goat.pop()} and ${this.goat.pop()}; the computer chose ${this.choice}; these are the doors that can be opened: ${this.doorsThatCanBeOpened}`);
+  this.doorsThatCanBeOpened = [1, 2, 3].filter(el => el !== this.choice)
+    .filter(el => el !== this.car);
+  this.openDoor = function () {
+    if (this.doorsThatCanBeOpened.length === 1) { return this.doorsThatCanBeOpened[0]; }
+    return this.doorsThatCanBeOpened[Math.round(Math.random())];
+  };
+  this.play = console.log(`car is ${this.car}; goats are ${this.goat.pop()} and ${this.goat.pop()}; the computer chose ${this.choice}; these are the doors that can be opened: ${this.doorsThatCanBeOpened}; opened ${this.openDoor()}`);
 }
