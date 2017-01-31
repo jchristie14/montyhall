446bb1c0cf1e551a411bc5f0005dfc978f2b4fef
John Christie <omeara.christie@gmail.com>
2017-01-31 11:59:46 -0500

    Move d3 to game.js file

--- scripts/game.js
+++ scripts/game.js
@@ -40,6 +40,50 @@ function playMultiple(times) {
   return [currentCounter.switchWin, currentCounter.stayWin];
 }
 
+function chartGames(games){
+
+var data = playMultiple(games);
+
+var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6"]);
+
+var arc = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var labelArc = d3.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function(d) { return d; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", `translate(${width/2},${height/2})`);
+
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { if(d.index === 0) { return `${d.data} people won if they switched.`} else { return `${d.data} people won if they stayed`}; });
+}
+
 
 
 
