88db6625f2092ab9b7335de52e030d2ed68e79c4
John Christie <omeara.christie@gmail.com>
2017-02-03 15:23:54 -0500

    Begin moving to node

---
+++ site/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+const handlebars = require('express-handlebars')
+  .create({ defaultLayout: 'main' });
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.use((req, res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:'+app.get('port')+'; press Ctrl-C to terminate.');
+});
